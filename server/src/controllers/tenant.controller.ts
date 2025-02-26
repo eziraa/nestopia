af67c2df39b30654ea6935bd9166fa1286106215
Ezira Tigab <ezratgab@gmail.com>
2025-02-26 13:25:06 +0300

    feat: adding tenant controller method to update tenant

--- server/src/controllers/tenant.controller.ts
+++ server/src/controllers/tenant.controller.ts
@@ -44,5 +44,33 @@ export class TenantController {
         message: `Error creating tenant ${error.message}`,
       });
     }
-  }
+    }
+    
+    public async updateTenant(req: Request, res: Response): Promise<void> {
+        try
+        {
+            // Getting tenant cognito id from request parameters
+            const { cognitoId } = req.params;
+            // Getting tenant data
+            const { name, email, phoneNumber } = req.body as ICTenant;
+
+            // Updating tenant
+            const updatedTenant = await prisma.tenant.update({
+                where: { cognitoId },
+                data: {
+                    name,
+                    email,
+                    phoneNumber,
+                },
+            });
+
+            // Sending response
+            res.json(updatedTenant);
+        } catch (error: any)
+        {
+            res.status(500).json({
+                message: `Error updating tenant ${error.message}`,
+            });
+        }
+    }
 }
