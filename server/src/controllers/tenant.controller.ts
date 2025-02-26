c1480a4909faa542b892d9571eafdc89c38086fa
Ezira Tigab <ezratgab@gmail.com>
2025-02-26 12:38:39 +0300

    feat: create tenant controller to get tenants and create tenant

---
+++ server/src/controllers/tenant.controller.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { ICTenant } from "../types/tenantTypes";
+const prisma = new PrismaClient();
+export class TenantController {
+  public async listTetents(req: Request, res: Response): Promise<void> {
+    try {
+      // Getting tenants
+      const tenants = await prisma.tenant.findMany({
+        include: {
+          properties: true,
+          applications: true,
+        },
+      });
+
+      // Sending response
+      res.json(tenants);
+    } catch (error: any) {
+      res.status(500).json({
+        message: `Error fetching tenants ${error.message}`,
+      });
+    }
+  }
+
+  public async createTenant(req: Request, res: Response): Promise<void> {
+    try {
+      // Getting tenant data
+      const { cognitoId, name, email, phoneNumber } = req.body as ICTenant;
+
+      // Creating tenant
+      const tenant = await prisma.tenant.create({
+        data: {
+          cognitoId,
+          name,
+          email,
+          phoneNumber,
+        },
+      });
+
+      // Sending response
+      res.status(201).json(tenant);
+    } catch (error: any) {
+      res.status(500).json({
+        message: `Error creating tenant ${error.message}`,
+      });
+    }
+  }
+}
