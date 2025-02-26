bb4f3f85e31f07be8cf78104377f7f7a5c2d0adb
Ezira Tigab <ezratgab@gmail.com>
2025-02-26 15:05:55 +0300

    feat: create lease controller and adding methods

---
+++ server/src/controllers/lease.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export class LeaseController {
+  public async listLeases(req: Request, res: Response): Promise<void> {
+    try {
+      const leases = await prisma.lease.findMany({
+        include: {
+          tenant: true,
+          property: true,
+        },
+      });
+
+      res.json(leases);
+    } catch (err: any) {
+      res.status(200).json({
+        message: `Failed to fetch leases ${err.message}`,
+      });
+    }
+  }
+
+  public async getLeasePayments(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const payments = await prisma.payment.findMany({
+        where: { leaseId: Number(id) },
+      });
+      res.json(payments);
+    } catch (error: any) {
+      res.status(500).json({
+        message: `Error retrieving lease payments: ${error.message}`,
+      });
+    }
+  }
+}
