66a4eb9e5303794d372740531a96b2e8ea5d4b2e
Ezira Tigab <ezratgab@gmail.com>
2025-02-27 11:20:11 +0300

    feat: adding auth router to manage authentication

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,40 @@
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import session from "express-session";
+/* ROUTE IMPORT */
+import authrouter from "./routes/auth.route";
 
-const startApp = () => {
-  /* CONFIGURATIONS */
-  dotenv.config();
-  const app = express();
-  app.use(express.json());
-  app.use(helmet());
-  app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-  app.use(morgan("common"));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cors());
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret_key90nfi2",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
 
-  /* ROUTES */
-  app.get("/", (req, res) => {
-    res.send("This is home route");
-  });
+app.use("/auth", authrouter);
 
-  /* SERVER */
-  const port = Number(process.env.PORT) || 3002;
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running on port ${port}`);
-  });
-};
-
-startApp();
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
