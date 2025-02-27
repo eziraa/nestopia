c69eeb8f2b4cc0eda09f099f804f0671c022125f
Ezira Tigab <ezratgab@gmail.com>
2025-02-27 11:19:03 +0300

    feat: create auth middleare

---
+++ server/src/middleware/authMiddleware.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Role } from "../enums/RoleEnums";
+
+interface DecodedToken extends JwtPayload {
+  sub: string;
+  "custom:role"?: string;
+}
+
+interface User {
+  id: string;
+  role: Role;
+  password?: string;
+  name?: string;
+  email?: string;
+  confirmPassword?: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
+
+export const authMiddleware = (allowedRoles: Role[]) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+
+    try {
+      const decoded = jwt.decode(token) as DecodedToken;
+      const userRole = (decoded["custom:role"] || Role.NONE) as Role;
+      req.user = {
+        id: decoded.sub,
+        role: userRole,
+      };
+
+      const hasAccess = allowedRoles.includes(userRole);
+      if (!hasAccess) {
+        res.status(403).json({ message: "Access Denied" });
+        return;
+      }
+    } catch (err) {
+      console.error("Failed to decode token:", err);
+      res.status(400).json({ message: "Invalid token" });
+      return;
+    }
+
+    next();
+  };
+};
