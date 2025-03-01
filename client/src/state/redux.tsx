5c559af018ddbdc19f88deb7841b819afbeed8b3
Ezira Tigab <ezratgab@gmail.com>
2025-03-01 11:33:43 +0300

    feat: create redux store

---
+++ client/src/state/redux.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useRef } from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import globalReducer from "@/state";
+import { api } from "@/state/api";
+import authReducer from "@/state/slices/auth.slice";
+import { AuthApi } from "./auth.api";
+
+/* REDUX STORE */
+const rootReducer = combineReducers({
+  global: globalReducer,
+  [api.reducerPath]: api.reducer,
+  [AuthApi.reducerPath]: AuthApi.reducer,
+  auth: authReducer,
+});
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(api.middleware).concat(AuthApi.middleware),
+
+  });
+};
+
+/* REDUX TYPES */
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+/* PROVIDER */
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current)
+  {
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
+  }
+  return <Provider store={storeRef.current}>{children}</Provider>;
+}
