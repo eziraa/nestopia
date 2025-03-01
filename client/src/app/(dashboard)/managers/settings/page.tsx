667309b6b6453d699cff7e1b3baf2e632c4e5cb9
Ezira Tigab <ezratgab@gmail.com>
2025-03-01 11:28:27 +0300

    feat: create mager settings page

---
+++ client/src/app/(dashboard)/managers/settings/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import SettingsForm from "@/components/SettingsForm";
+import {
+  useUpdateManagerSettingsMutation,
+} from "@/state/api";
+import { useGetCurrentUserQuery } from "@/state/auth.api";
+import React from "react";
+
+const ManagerSettings = () => {
+  const { data: { user: authUser } = {}, isLoading } = useGetCurrentUserQuery();
+  const [updateManager] = useUpdateManagerSettingsMutation();
+
+  if (isLoading) return <>Loading...</>;
+
+  const initialData = {
+    name: authUser?.name,
+    email: authUser?.email,
+    phoneNumber: authUser?.phoneNumber,
+  };
+
+  const handleSubmit = async (data: typeof initialData) => {
+    await updateManager({
+      cognitoId: authUser?.id,
+      ...data,
+    });
+  };
+
+  return (
+    <SettingsForm
+      initialData={initialData}
+      onSubmit={handleSubmit}
+      userType="manager"
+    />
+  );
+};
+
+export default ManagerSettings;
