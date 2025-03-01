4a794042b5cad1fbb750bad956b3c0e741f10d67
Ezira Tigab <ezratgab@gmail.com>
2025-03-01 10:04:14 +0300

    feat: create signup and settings zod schema for validation purpose

---
+++ client/src/lib/schemas.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+export const signUpSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(8, "Password lenth should be at least 8 "),
+  confirmPassword: z.string().min(8, "Confirmed length should be at least 8"),
+});
+
+export const settingsSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email address"),
+  phoneNumber: z.string().min(10, "Phone number must be at least 10 digits"),
+});
+
+export type SettingsFormData = z.infer<typeof settingsSchema>;
