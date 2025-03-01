21abc1b95ca9e9ffd543ab3588b73f50eded0eaa
Ezira Tigab <ezratgab@gmail.com>
2025-03-01 09:39:12 +0300

    feat: create none dashboard layout

--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -2,34 +2,9 @@
 
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import { useGetCurrentUserQuery } from "@/state/auth.api";
-import { usePathname, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { data: user, isLoading: authLoading } = useGetCurrentUserQuery();
-  const router = useRouter();
-  const pathname = usePathname();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (user)
-    {
-      const userRole = user.user.role?.toLowerCase();
-      if (
-        (userRole === Role.Manager && pathname.startsWith("/search")) ||
-        (userRole === Role.Manager && pathname === "/")
-      )
-      {
-        router.push("/managers/properties", { scroll: false });
-      } else
-      {
-        setIsLoading(false);
-      }
-    }
-  }, [user, router, pathname]);
-
-  if (authLoading || isLoading) return <>Loading...</>;
 
   return (
     <div className="h-full w-full">
