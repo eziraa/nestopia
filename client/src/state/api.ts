ea49508781a9add63708b740eab8fb6a0a52814e
Ezira Tigab <ezratgab@gmail.com>
2025-03-01 10:54:55 +0300

    feat: create map component

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,6 +1,7 @@
-import { withToast } from "@/lib/utils";
-import { Tenant } from "@/types/prismaTypes";
+import { cleanParams, withToast } from "@/lib/utils";
+import { Property, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { FiltersState } from ".";
 
 enum ApiTags {
   MANAGERS = "Managers",
@@ -36,7 +37,46 @@ export const api = createApi({
         });
       },
     }),
+    getProperties: build.query<
+      Property[],
+      Partial<FiltersState> & { favoriteIds?: number[] }
+    >({
+      query: (filters) => {
+        const params = cleanParams({
+          location: filters.location,
+          priceMin: filters.priceRange?.[0],
+          priceMax: filters.priceRange?.[1],
+          beds: filters.beds,
+          baths: filters.baths,
+          propertyType: filters.propertyType,
+          squareFeetMin: filters.squareFeet?.[0],
+          squareFeetMax: filters.squareFeet?.[1],
+          amenities: filters.amenities?.join(","),
+          availableFrom: filters.availableFrom,
+          favoriteIds: filters.favoriteIds?.join(","),
+          latitude: filters.coordinates?.[1],
+          longitude: filters.coordinates?.[0],
+        });
+
+        return { url: "properties", params };
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({
+                type: ApiTags.PROPERTIES as const,
+                id,
+              })),
+              { type: ApiTags.PROPERTIES, id: "LIST" },
+            ]
+          : [{ type: ApiTags.PROPERTIES, id: "LIST" }],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          error: "Failed to fetch properties.",
+        });
+      },
+    }),
   }),
 });
 
-export const { useUpdateTenantSettingsMutation } = api;
+export const { useUpdateTenantSettingsMutation, useGetPropertiesQuery } = api;
