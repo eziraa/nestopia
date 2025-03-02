d4ad300b02b1f5a8f86ea45758611289270adc29
Ezira Tigab <ezratgab@gmail.com>
2025-03-02 10:10:41 +0300

    feat: create page  to display current residences

---
+++ client/src/app/(dashboard)/tenants/residences/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Card from "@/components/Card";
+import Header from "@/components/Header";
+import Loading from "@/components/Loading";
+import {
+  useGetCurrentResidencesQuery,
+  useGetTenantQuery,
+} from "@/state/api";
+import { useGetCurrentUserQuery } from "@/state/auth.api";
+import React from "react";
+
+const Residences = () => {
+  const { data: { user: authUser } = {} } = useGetCurrentUserQuery();
+  const { data: tenant } = useGetTenantQuery(
+    authUser?.id || "",
+    {
+      skip: !authUser?.id,
+    }
+  );
+
+  const {
+    data: currentResidences,
+    isLoading,
+    error,
+  } = useGetCurrentResidencesQuery(authUser?.id || "", {
+    skip: !authUser?.id,
+  });
+
+  if (isLoading) return <Loading />;
+  if (error) return <div>Error loading current residences</div>;
+
+  return (
+    <div className="dashboard-container">
+      <Header
+        title="Current Residences"
+        subtitle="View and manage your current living spaces"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {currentResidences?.map((property) => (
+          <Card
+            key={property.id}
+            property={property}
+            isFavorite={tenant?.favorites.includes(property.id) || false}
+            onFavoriteToggle={() => { }}
+            showFavoriteButton={false}
+            propertyLink={`/tenants/residences/${property.id}`}
+          />
+        ))}
+      </div>
+      {(!currentResidences || currentResidences.length === 0) && (
+        <p>You don&lsquo;t have any current residences</p>
+      )}
+    </div>
+  );
+};
+
+export default Residences;
