feed2903046e9716c82eaa5e0f365f57b5b88331
Ezira Tigab <ezratgab@gmail.com>
2025-03-02 22:51:27 +0300

    update: update auth route to fix authentication related isseus

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { Role } from "../enums/RoleEnums";
 const router = Router();
 router.post("/signin", AuthController.signin);
 router.post("/signup", AuthController.signup);
-router.post("/logout", AuthController.logout);
-router.get("/current", AuthController.getCurrUser);
+router.post(
+  "/logout",
+  authMiddleware([Role.MANAGER, Role.TENANT, Role.NONE]),
+  AuthController.logout
+);
+router.get(
+  "/current",
+  authMiddleware([Role.MANAGER, Role.TENANT, Role.NONE]),
+  AuthController.getCurrUser
+);
 
 export default router;
+
