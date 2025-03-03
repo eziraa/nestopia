90eb1324f62610de4ae1753503606a41af110e22
Ezira Tigab <ezratgab@gmail.com>
2025-03-03 19:03:58 +0300

    update: auth middlware send cookies to client and to get current user from request

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { Role } from "../enums/RoleEnums";
+import { Manager, Tenant } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 interface DecodedToken extends JwtPayload {
   sub: string;
   "custom:role"?: string;
@@ -20,29 +23,69 @@ interface User {
 declare global {
   namespace Express {
     interface Request {
-      user?: User;
+      user?: (User | Tenant | Manager) & { role: Role };
+      cookie?: { token: string };
     }
   }
 }
 
 export const authMiddleware = (allowedRoles: Role[]) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    const token = req.headers.authorization?.split(" ")[1];
-
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const cookies = req.headers.cookie?.split("; ").reduce((acc, cookie) => {
+      const [name, value] = cookie.split("=");
+      acc[name] = value;
+      return acc;
+    }, {} as Record<string, string>);
+    const token = cookies?.token;
     if (!token) {
-      res.status(401).json({ message: "Unauthorized" });
+      res.status(401).json({ message: "Unauthorized please login" });
       return;
     }
 
     try {
-      const decoded = jwt.decode(token) as DecodedToken;
-      const userRole = (decoded["custom:role"] || Role.NONE) as Role;
-      req.user = {
-        id: decoded.sub,
-        role: userRole,
+      if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined");
+      }
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as {
+        id: number;
+        role: Role;
       };
+      let role = Role.MANAGER;
 
-      const hasAccess = allowedRoles.includes(userRole);
+      let user = await prisma.manager.findUnique({ where: { id: decoded.id } });
+      if (!user) {
+        user = await prisma.tenant.findUnique({ where: { id: decoded.id } });
+        role = Role.TENANT;
+      }
+      req.user = {
+        id: decoded.id,
+        role: role,
+        cognitoId: user?.cognitoId || "",
+        name: user?.name || "",
+        email: user?.email || "",
+        phoneNumber: user?.phoneNumber || "",
+        password: "",
+      };
+      // Set token in HTTP-only cookie
+      res.cookie("token", token, {
+        httpOnly: true, // Prevents client-side access
+        secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+        sameSite: "strict", // CSRF protection
+        maxAge: 3600000, // 1 hour
+      });
+      console.log("User:", req.user);
+
+      if (!req.user) {
+        res.status(401).json({ message: "Unauthorized please login" });
+        return;
+      } else {
+        next();
+      }
+      const hasAccess = allowedRoles.includes(req.user.role);
       if (!hasAccess) {
         res.status(403).json({ message: "Access Denied" });
         return;
@@ -52,7 +95,5 @@ export const authMiddleware = (allowedRoles: Role[]) => {
       res.status(400).json({ message: "Invalid token" });
       return;
     }
-
-    next();
   };
 };
