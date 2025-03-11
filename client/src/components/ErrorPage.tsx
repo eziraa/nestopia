0d0ae247a17cf62dc1abecff924fbe5b2baa1a19
eziraa <ezratgab@gmail.com>
2025-03-11 09:03:38 +0300

    update: update error page to fix style issues

--- client/src/components/ErrorPage.tsx
+++ client/src/components/ErrorPage.tsx
@@ -12,7 +12,7 @@ interface Props{
 const ErrorPage = ({children, title, status, callbackURL}:Props) => {
     const router = useRouter()
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-red-400 via-pink-500 to-purple-600">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-200 via-red-100 to-slate-200">
 {children 
 ? 
 (
@@ -21,9 +21,9 @@ const ErrorPage = ({children, title, status, callbackURL}:Props) => {
 (
     <div className="flex">
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-9xl font-bold text-white">{status || 404}</h1>
-            <h2 className="text-3xl font-bold text-white">{title || "Some thing went to wrong"}</h2>
-            <button className="mt-5 bg-white text-black px-4 py-2 rounded-md" onClick={() => callbackURL && router.push(callbackURL)}>Go back</button>
+            <h1 className="text-9xl font-bold text-red-400">{status }</h1>
+            <h2 className="text-3xl font-bold text-red-400">{title || "Some thing went to wrong"}</h2>
+            <button className="mt-5  bg-red-200 text-red-500/90 hover:text-red-500 px-4 py-2 rounded-md" onClick={() => callbackURL && router.push(callbackURL)}>Go back</button>
         </div>
     </div>
 )}
